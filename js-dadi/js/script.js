ba6d02e1ad343ded5cd4925184cb242f09b9734d
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-01 15:48:58 +0100

    inserito gioco dadi

--- js-dadi/js/script.js
+++ js-dadi/js/script.js
@@ -1,1 +1,14 @@
-alert();
+//genero numero random per me
+var mynumber = Math.floor( Math.random()*6 );
+console.log(mynumber);
+//genero numero random per l'avversario
+var myenemy = Math.floor( Math.random()*6 );
+console.log(myenemy);
+//determino le condizioni per vittoria/sconfitta/pareggio
+if ( mynumber > myenemy ) {
+  alert('You win!!!GG');
+} else if ( mynumber < myenemy ) {
+  alert('You looose!');
+} else if ( mynumber == myenemy ) {
+  alert('Draw! Try again');
+}

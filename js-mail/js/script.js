baf6c70d3beacdeaa0f32179d8161b29df5818d9
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-01 15:32:11 +0100

    inserite condizioni verifica e-mail

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,1 +1,13 @@
-alert();
+//Chiedere all'utente la Mail
+var mail = prompt('Qual è la sua e-mail?');
+//Creo array con le e-mail
+var email = ['gianluca.forgia@gmail.com', 'mario.rossi@gmail.com', 'laura.mancini@gmail.com', 'antonio.bianchi@hotmail.it']
+//Verifico che la mail sia nella lista
+for (var i = 0; i < email.length; i++) {
+  console.log( email[i]);
+  if ( mail == email[i] ) {
+    alert('e-mail inserita correttamente');
+  } else {
+    alert('la tua email non risulta nella lista');
+  }
+}

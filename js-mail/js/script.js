fd8c6e8e7d2c76b719b8cb40b31facaf1215ed8e
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-01 17:37:45 +0100

    escludo statement break

--- js-mail/js/script.js
+++ js-mail/js/script.js
@@ -1,15 +1,19 @@
-//Chiedere all'utente la Mail
-var mail = prompt('Qual è la sua e-mail?');
+ //Chiedere all'utente la Mail
+ var mail = prompt('Qual è la sua e-mail?');
 //Creo array con le e-mail
-var email = ['gianluca.forgia@gmail.com', 'mario.rossi@gmail.com', 'laura.mancini@gmail.com', 'antonio.bianchi@hotmail.it']
-//Verifico che la mail sia nella lista
-for (var i = 0; i < email.length; i++) {
-  console.log( email[i]);
+ var email = ['gianluca.forgia@gmail.com', 'mario.rossi@gmail.com', 'laura.mancini@gmail.com', 'antonio.bianchi@hotmail.it']
+ //Creo la variabile emailValida
+ var emailValida = 0;
+ //Identifico il mio array
+ for (var i = 0; i < email.length; i++) {
+   console.log( email[i]);
   if ( mail == email[i] ) {
-    alert('e-mail inserita correttamente!');
-    break;
-  } else {
-    alert('la tua email non risulta nella lista... ci dispiace.');
-    break;
+    emailValida = 1;
   }
+ }
+//Pongo le condizioni per emailValida
+if ( emailValida == 1) {
+  alert('E-mail inserita correttamente!!');
+} else {
+  alert('Non risulta nella lista la mail che hai inserito.. ci dispiace.');
 }
